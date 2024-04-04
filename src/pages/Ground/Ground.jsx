ac87c593fd0ca172e87cd0ae19709116d18d5a32
comet <comet2667@naver.com>
2024-04-04 16:17:23 +0900

    착수 기능 구현

--- src/pages/Ground/Ground.jsx
+++ src/pages/Ground/Ground.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { insertStone } from "../../api/user/user";
 
 function Ground() {
     const [co, setCo] = useState([]);
-
-    const ground = [];
+    const ref = useRef([]);
+    let ground = [];
     let x = [];
     const createGround = () => {
+        ground = [];
         for (let i = 0; i < 19; i++) {
             x = [];
             for (let j = 0; j < 19; j++) {
@@ -14,29 +15,40 @@ function Ground() {
             }
             ground.push(x);
         }
-        setCo(ground);
     }
     useEffect(() => {
         createGround();
+        setCo(ground);
     }, [])
 
-    const insert = async (idxX, idxY)=>{
+    const insert = async (idxX, idxY) => {
         const result = await insertStone({
-            x:idxX,
-            y:idxY,
+            x: idxX,
+            y: idxY,
         });
         console.log(result);
+        ref.current[idxX + (idxY*18)].disabled = true;
     }
 
     return (
         <div>
             <table>
+                <tr>
+                    <td>no</td>
+                    {co.map((c, idx)=>{
+                        return (
+                            <td key={idx}>{idx}</td>
+                        );
+                    })}
+                </tr>
                 {co.map((y, idxY) => {
                     return (
-                        <tr key={idxY}>
-                            {y.map((x, idxX)=>{
-                                return(
-                                <td key={idxX}><button onClick={()=>insert(idxX, idxY)}>O</button></td>
+                        <tr key={idxY}>{idxY}
+                            {y.map((x, idxX) => {
+                                return (
+                                    <td key={idxX}><button 
+                                            onClick={() => insert(idxX, idxY)}
+                                            ref={(el)=>{ref.current[idxX + (idxY*18)] = el;}}>O</button></td>
                                 );
                             })}
                         </tr>
